1326529dd6b218e69a30e23906c836811bdd40be
Sergions1 <sergionunezsierra1@gmail.com>
2026-03-13 12:49:55 +0100

    Feat: create de project structure

---
+++ src/main/java/com/mgcss/domain/Cliente.java
@@ -0,0 +1,10 @@
+package com.mgcss.domain;
+
+import com.mgcss.domain.Enums.TipoCliente;
+
+public class Cliente {
+	private Long id;
+	private String nombre;
+	private String email;
+	private TipoCliente tipo;
+}
--- src/main/java/com/mgcss/domain/Solicitud.java
+++ src/main/java/com/mgcss/domain/Solicitud.java
@@ -2,10 +2,18 @@ package com.mgcss.domain;
 
 import java.util.Date;
 
+import com.mgcss.domain.Enums.Estado;
+
 public class Solicitud {
 	private Long id;
-	private int estado;
+	private Cliente cliente;
+	private String descripcion;
 	private Date fechaCreacion;
+	private Estado estado;
+	private Tecnico tecnico;
+	private Date fechaCierre;
+	
+	
 	
 	public void cerrar() {}
 }
