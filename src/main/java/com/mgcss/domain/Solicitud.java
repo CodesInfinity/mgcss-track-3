c63011835965baf1e5a89f51efafdb2007b2406f
Sergions1 <sergionunezsierra1@gmail.com>
2026-03-13 12:36:02 +0100

    Feat: create de project structure

---
+++ src/main/java/com/mgcss/domain/Solicitud.java
@@ -0,0 +1,11 @@
+package com.mgcss.domain;
+
+import java.util.Date;
+
+public class Solicitud {
+	private Long id;
+	private int estado;
+	private Date fechaCreacion;
+	
+	public void cerrar() {}
+}
