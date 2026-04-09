81cbc96676305617ac533b9819f5ebe4c17ff0d1
Sergions1 <sergionunezsierra1@gmail.com>
2026-04-10 00:51:03 +0200

    Crea test probando getters y setters

---
+++ src/test/java/com/mgcss/domain/tests/GetterYSetterClienteTest.java
@@ -0,0 +1,28 @@
+package com.mgcss.domain.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.mgcss.domain.Cliente;
+import com.mgcss.domain.Enums.TipoCliente;
+
+
+class GetterYSetterClienteTest {
+
+	@Test
+	void test() {
+		String nombre = "Juan Perez";
+        String email = "juan@example.com";
+        TipoCliente tipo = TipoCliente.STANDARD;
+
+        Cliente cliente = new Cliente(nombre, email, tipo);
+        
+        assertEquals(nombre, cliente.getNombre(), "El nombre no coincide");
+        assertEquals(email, cliente.getEmail(), "El email no coincide");
+        assertEquals(tipo, cliente.getTipo(), "El tipo de cliente no coincide");
+        
+        assertNotNull(cliente.getId(), "El ID debería haberse generado automáticamente");
+	}
+
+}
